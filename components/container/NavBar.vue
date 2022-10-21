7ea765826e87f837b32d146b8db3f5414005a390
Umayangi Ekanayake <89120566+umayangi12@users.noreply.github.com>
2022-10-21 17:54:17 +0530

    error fixed

--- components/container/NavBar.vue
+++ components/container/NavBar.vue
@@ -2,7 +2,7 @@
   <nav class="relative shadow-lg py-9 bg-[#080A3C] padding-box h-80px w-1140px">
     <div class="justify-between w-full px-6 font-bold text-center">
       <div class="content-center p-2 text-2xl font-bold text-center text-white">
-        simpleBooks
+        simplebooks
       </div>
     </div>
   </nav>
--- pages/bstate.vue
+++ pages/bstate.vue
@@ -9,9 +9,50 @@
         <div class="justify-between text-xs font-bold text-gray-400 px-96">
           <h3>From the below list, select what fits you best</h3>
         </div>
-        <RadioBtn1></RadioBtn1>
-        <RadioBtn2></RadioBtn2>
-        <RadioBrn3></RadioBrn3>
+        <div class="flex items-center mt-4 mb-4 px-96">
+          <input
+            id="default-radio-1"
+            type="radio"
+            value=""
+            name="default-radio"
+            class="w-4 h-4 bg-gray-100 border-gray-300 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+          />
+          <label
+            for="default-radio-1"
+            class="ml-2 text-sm font-bold text-gray-900 dark:text-gray-900"
+            >I want to incorporate my business with Simplebooks</label
+          >
+        </div>
+        <div class="flex items-center mb-4 px-96">
+          <input
+            checked
+            id="default-radio-2"
+            type="radio"
+            value=""
+            name="default-radio"
+            class="w-4 h-4 bg-gray-100 border-gray-300 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+          />
+          <label
+            for="default-radio-2"
+            class="ml-2 text-sm font-bold text-gray-900 dark:text-gray-900"
+            >My business is already incorparated</label
+          >
+        </div>
+        <div class="flex items-center mb-4 px-96">
+          <input
+            checked
+            id="default-radio-3"
+            type="radio"
+            value=""
+            name="default-radio"
+            class="w-4 h-4 bg-gray-100 border-gray-300 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+          />
+          <label
+            for="default-radio-3"
+            class="ml-2 text-sm font-bold text-gray-900 dark:text-gray-900"
+            >I am not looking to incorporate my business</label
+          >
+        </div>
       </div>
       <div class="flex justify-center mr-8">
         <NextBtn></NextBtn>
@@ -23,17 +64,12 @@
 <script>
 import NavBar from "../components/container/NavBar.vue";
 import NextBtn from "../components/buttons/NextBtn.vue";
-import RadioBtn1 from "../components/buttons/RadioBtn1.vue";
-import RadioBtn2 from "../components/buttons/RadioBtn1.vue";
-import RadioBtn3 from "../components/buttons/RadioBtn3.vue";
 
 export default {
   components: {
     NavBar,
     NextBtn,
-    RadioBtn1,
-    RadioBtn2,
-    RadioBtn3,
+   
   },
 };
 </script>
